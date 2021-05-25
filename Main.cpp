7d768551afa828f2f16f514a02a6252fd4ca7406
intns <84647527+intns@users.noreply.github.com>
2021-05-25 03:48:33 +0100

    Remove overdocumentation of main function

--- Main.cpp
+++ Main.cpp
@@ -6,22 +6,17 @@
 
 int main(int argc, char** argv)
 {    
-    // Initialise scene with objects
     World scene;
-    auto& objects = scene.getObjects();
-    // Add SphereObject
+    std::vector<Object*>& objects = scene.getObjects();
     objects.push_back(
         new SphereObject(5, Vec3(0, 0, 5)) // Sphere with radius 5, and XYZ position (0, 0, 5)
     );
 
-    // Create instance of image creator
     ppm::Image image(256, 256);
-    // Render scene to image
     scene.render(image);
-    // Output scene
     std::cout << image;
 
-    // Clean up scene
+    // Clean up scene, prevent memory leaks
     for (std::size_t i = 0; i < objects.size(); i++)
     {
         delete objects[i];
