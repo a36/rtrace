b6b3f521134cb2001c2dc52006e24c02bd3a9ca5
intns <84647527+intns@users.noreply.github.com>
2021-05-25 18:30:40 +0100

    Remove Vec3Tests, make Ray's member variables homogenous in terms of initialisation compared to the main project

--- Math/Ray.cpp
+++ Math/Ray.cpp
@@ -1,11 +1,5 @@
 #include <Math/Ray.h>
 
-Ray::Ray()
-    : m_origin(0, 0, 0)
-    , m_direction(0, 0, -1)
-{
-}
-
 Ray::Ray(Vec3 origin, Vec3 direction)
     : m_origin(origin)
     , m_direction(direction)
--- Math/Ray.h
+++ Math/Ray.h
@@ -8,8 +8,9 @@
 
 class Ray {
 public:
-    Ray();
+    Ray() = default;
     Ray(Vec3 origin, Vec3 direction);
+    ~Ray() = default;
 
     inline const Vec3& getOrigin() const { return m_origin; }
     inline const Vec3& getDirection() const { return m_direction; }
@@ -18,8 +19,8 @@ public:
     inline const Object* getIntersection() const { return m_intersect; }
 
 private:
-    Vec3 m_origin;
-    Vec3 m_direction;
+    Vec3 m_origin = {0, 0, 0};
+    Vec3 m_direction = {0, 0, -1};
 
     Object* m_intersect = nullptr;
 };
