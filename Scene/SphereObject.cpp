302be7889b5f2e56763fbe3c8eeb2dfc913365d8
a36 <a36vodaphone@outlook.com>
2021-05-29 22:36:05 -0600

    Update intersect function

--- Scene/SphereObject.cpp
+++ Scene/SphereObject.cpp
@@ -9,5 +9,14 @@ SphereObject::SphereObject(double r, Vec3 position)
 
 bool SphereObject::intersect(Ray& r)
 {
-    return false;
+    Vec3 oc = this->m_position - r.getOrigin(); // Not sure if better way to access m_position
+    double a = oc.dot(oc, r.getDirection()); // Not sure if we can call dot without doing oc.dot
+    if (a < 0)
+        return false;
+    double b = oc.dot(oc, oc) - a*a;
+    if (b > this->m_radius * this->m_radius) // Just make radius^2 a member variable instead
+        return false;
+    double c = sqrt(this->m_radius * this->m_radius - b); // Will use c in World.cpp to calculate distance
+
+    return true;
 }
