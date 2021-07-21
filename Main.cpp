9df392269be5fc8d3fb3c41041d0df49d1f6362d
intns <84647527+intns@users.noreply.github.com>
2021-07-22 00:20:17 +0100

    Main now doesn't output to stdout, but instead a file named out.ppm

--- Main.cpp
+++ Main.cpp
@@ -1,17 +1,18 @@
+#include <fstream>
 #include <iostream>
 
-#include <Scene/World.h>
+#include <Image/PPM.h>
 #include <Scene/Camera.h>
 #include <Scene/SphereObject.h>
-#include <Image/PPM.h>
+#include <Scene/World.h>
 #include <Util/Color.h>
 
 int main(int argc, char** argv)
-{    
+{
     World scene;
     std::vector<Object*>& objects = scene.getObjects();
     objects.push_back(
-        // Camera angle seems a bit buggy right now, SphereObject with coords (0, 0, -10) 
+        // Camera angle seems a bit buggy right now, SphereObject with coords (0, 0, -10)
         // should  appear in the center of screen, but instead appears to the left.
         new SphereObject(5, Vec3(7, 0, -17.5)) // Sphere with radius 5, and XYZ position (7, 0, -17.5)
     );
@@ -22,11 +23,15 @@ int main(int argc, char** argv)
 
     scene.render(image, camera);
 
-    std::cout << image;
+    // Output image to out.ppm
+    std::ofstream out_ppm("out.ppm");
+    if (out_ppm.is_open()) {
+        out_ppm << image;
+        out_ppm.close();
+    }
 
     // Clean up scene, prevent memory leaks
-    for (std::size_t i = 0; i < objects.size(); i++)
-    {
+    for (std::size_t i = 0; i < objects.size(); i++) {
         delete objects[i];
     }
 
