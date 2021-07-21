95a09bea924241c84c5c642511ba255aa7c369ef
intns <84647527+intns@users.noreply.github.com>
2021-07-22 00:04:08 +0100

    Add 64-bit types to Types.h

--- Types.h
+++ Types.h
@@ -3,10 +3,12 @@
 
 #include <cstdint>
 
+typedef std::uint64_t u64;
 typedef std::uint32_t u32;
 typedef std::uint16_t u16;
 typedef std::uint8_t u8;
 
+typedef std::int64_t s64;
 typedef std::int32_t s32;
 typedef std::int16_t s16;
 typedef std::int8_t s8;
